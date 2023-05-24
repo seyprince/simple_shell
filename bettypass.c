18508b243d714a660caa0c31e663e31ce66fb5bb
seyprince <princesey@outlook.com>
2023-05-24 22:44:30 +0000

    betty pass code with Authors

---
+++ bettypass.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 5;
+	int y = 7;
+	int sum;
+
+	sum = x + y;
+	printf("the sum of 5 and 7 is %d\n", sum);
+	return (0);
+}
