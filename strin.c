3e760f4d6449843c10ef8549ef46acb3e0fad820
seyprince <princesey@outlook.com>
2023-05-25 02:05:40 +0000

    strin.c

---
+++ strin.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _atoi - string to integer
+ *@s: string
+ *Return: integer
+ */
+int _atoi(char *s)
+{
+	int i, integer, sign = 1;
+
+	i = 0;
+	integer = 0;
+	while (!((s[i] >= '0') && (s[i] <= '9')) && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+		{
+			sign = sign * (-1);
+		}
+		i++;
+	}
+	while ((s[i] >= '0') && (s[i] <= '9'))
+	{
+		integer = (integer * 10) + (sign * (s[i] - '0'));
+		i++;
+	}
+	return (integer);
+}
