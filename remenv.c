b44daab3f94b0e5529c2e49edba778940e6725c4
seyprince <princesey@outlook.com>
2023-05-25 02:05:01 +0000

    remenv.c

---
+++ remenv.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _unsetenv - Remove environment variable
+ * @arv: array
+ */
+void _unsetenv(char **arv)
+{
+	int i, j;
+
+	if (!arv[1])
+	{
+		perror(_getenv("_"));
+		return;
+	}
+	for (i = 0; environ[i]; i++)
+	{
+		j = 0;
+		if (arv[1][j] == environ[i][j])
+		{
+			while (arv[1][j])
+			{
+				if (arv[1][j] != environ[i][j])
+					break;
+
+				j++;
+			}
+			if (arv[1][j] == '\0')
+			{
+				free(environ[i]);
+				environ[i] = environ[i + 1];
+				while (environ[i])
+				{
+					environ[i] = environ[i + 1];
+					i++;
+				}
+				return;
+			}
+		}
+	}
+}
