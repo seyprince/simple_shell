b77164d4d3da99200134670ecbf6f01a2e2ef7f1
seyprince <princesey@outlook.com>
2023-05-25 01:59:45 +0000

    stringsplit.c

---
+++ stringsplit.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+/**
+ * splitstring - makes array of pointers to words
+ * @str: the string
+ * @delim: the delimiter
+ * Return: arrays
+ */
+
+char **splitstring(char *str, const char *delim)
+{
+	int i, wn;
+	char **array;
+	char *token;
+	char *copy;
+
+	copy = malloc(_strlen(str) + 1);
+	if (copy == NULL)
+	{
+		perror(_getenv("_"));
+		return (NULL);
+	}
+	i = 0;
+	while (str[i])
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	copy[i] = '\0';
+
+	token = strtok(copy, delim);
+	array = malloc((sizeof(char *) * 2));
+	array[0] = _strdup(token);
+
+	i = 1;
+	wn = 3;
+	while (token)
+	{
+		token = strtok(NULL, delim);
+		array = _realloc(array, (sizeof(char *) * (wn - 1)), (sizeof(char *) * wn));
+		array[i] = _strdup(token);
+		i++;
+		wn++;
+	}
+	free(copy);
+	return (array);
+}
