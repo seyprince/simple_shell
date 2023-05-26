7235d7b7cfb9bab64abc391dde0b90668cb95760
seyprince <princesey@outlook.com>
2023-05-26 21:52:06 +0000

    integers

---
+++ pro14.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_number - print unsigned integer
+ * @n: Unisigned integer
+ * Return: nothing
+ */
+void print_number(unsigned int n)
+{
+	unsigned int x = n;
+
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
+/**
+ * print_number_in - print num
+ * @n: integer
+ * Return: nothing
+ */
+void print_number_in(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
