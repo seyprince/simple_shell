6c9ed80286a390ad1cf8c1ccbd3a7b4f5afceafa
seyprince <princesey@outlook.com>
2023-05-26 21:50:52 +0000

    also stringsbut copy

---
+++ pro11.c
@@ -0,0 +1,110 @@
+#include "main.h"
+/**
+ * _strcpy - copy to destination
+ * @dest: destination
+ * @src: source
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+dest[i] = '\0';
+return (dest);
+}
+/**
+ * _strcat - concatinate string
+ * @dest: first string
+ * @src:Second String
+ * Return: both strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *s = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (s);
+}
+/**
+ * _strchr - find character
+ * @s: search strg
+ * @c: character
+ * Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+
+	do		{
+
+		if (*s == c)
+			{
+			break;
+			}
+		}	while (*s++);
+
+return (s);
+}
+/**
+ * _strncmp - Compare strings
+ * @s1: string
+ * @s2: string
+ * @n: number of chrs
+ *
+ * Return: 1,0 success and fail respectively
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	if (s1 == NULL)
+		return (-1);
+	for (i = 0; i < n && s2[i]; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+/**
+ * _strdup - string duplication
+ * @str: string
+ * Return: string
+ */
+char *_strdup(char *str)
+{
+	size_t len, i;
+	char *str2;
+
+	len = _strlen(str);
+	str2 = malloc(sizeof(char) * (len + 1));
+	if (!str2)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		str2[i] = str[i];
+	}
+
+	return (str2);
+}
